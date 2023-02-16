59b2a03876cc91cb25e4ef2f95d90c6738e3fb07
MetacrafterChris <chris.gold@metacrafters.io>
2023-02-16 13:21:48 +0000

    init

--- playground.js
+++ playground.js
@@ -1,16 +1,15 @@
 /* 
 // Metacrafters - Beginning Javascript
+//
 // This is a javascript playground for testing your javascript code!
 // When you are ready to test, simply right click and select "Run Code"
 // to see the result print below
 // 
-//
 */
 
 // Enter your code below this line
 
-
 // Code example
-// for(var i=1; i<=10; i++) {
-//	 console.log(i);
-// }
+for(var i=1; i<=10; i++) {
+ console.log(i);
+}
